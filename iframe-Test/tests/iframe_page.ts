d7dc80a4218cadead189eaaba2d5f66a67e34fb5
dil-gfazekas <gfazekas@diligent.com>
2022-01-20 14:48:04 +0100

    fix: adding timeout for waiting

--- iframe-Test/tests/iframe_page.ts
+++ iframe-Test/tests/iframe_page.ts
@@ -1,11 +1,7 @@
 import { test, expect } from '@playwright/test'
 
 test('waiting', async ({ page }) => {
-
-    await page.goto('http://127.0.0.1:8081/');
-
+    await page.goto('http://127.0.0.1:8090/');
     const locator = page.frameLocator("xpath=//iframe[@id='ifrm']").locator("xpath=//button[@id='iframe_1_button']");
-
-    await expect(locator).toBeVisible();
-
+    await expect(locator).toBeVisible({ timeout: 10000 });
 });
